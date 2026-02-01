2db9c2f850d1d005dee36a354270c93771097c3a
LaetiMS <laetitia.sjt@gmx.ch>
2026-02-01 16:26:15 +0100

    change setup to ignore wandb folder to allow editable install

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 import setuptools
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setup(
+        # only include your nnunetv2 package and subpackages
+        packages=find_packages(include=["nnunetv2", "nnunetv2.*"]),
+    )
