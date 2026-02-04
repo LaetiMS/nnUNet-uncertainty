c387f0b73576493e89695b15b403d1832625d70c
LaetiMS <laetitia.sjt@gmx.ch>
2026-02-05 00:17:49 +0100

    new version works (old version = no global calibration calculation)  (currently no multiprocessing)

--- nnunetv2/evaluation/evaluate_calibration.py
+++ nnunetv2/evaluation/evaluate_calibration.py
@@ -59,7 +59,7 @@ def compute_calibration_metrics_gpt(gt_voxels, probs_eval, n_bins=15, eps=1e-8):
     return nll, brier, ece
 
 def compute_calibration_metrics(gt_voxels, probs_eval, n_bins=15, eps=1e-8):
-    nr_classes=probs_eval.shape[0]
+    nr_classes=probs_eval.shape[1]
     # --- Negative Log-Likelihood (multiclass) ---
     p_true = probs_eval[np.arange(gt_voxels.size), gt_voxels] #if probs_eval was transposed
     # p_true = probs_eval[gt_voxels, np.arange(gt_voxels.size)] # if probs_eval was not transposed
@@ -77,125 +77,125 @@ def compute_calibration_metrics(gt_voxels, probs_eval, n_bins=15, eps=1e-8):
     ece = compute_ece(confidence, correct, n_bins=n_bins)
     return nll, brier, ece
 
+# def compute_probabilistic_metrics_old(
+#         reference_file: str,
+#         probability_file: str,   # <-- NEW (saved softmax)
+#         image_reader_writer: BaseReaderWriter,
+#         labels_or_regions,
+#         ignore_label=None,
+#         n_bins=15,
+#         eps: float = 1e-8,
+# ) -> dict:
+#     """
+#     This function the compute_metrics equivalent for uncertainty calibration (evaluates the quality of probabilities):
+#         - loads GT
+#         - loads probability maps
+#         - Compute probabilistic / calibration metrics for segmentation::
+#             - NLL
+#             - Brier Score
+#             - ECE
+#             Metrics are computed voxel-wise and aggregated per case.
+#             Works for binary and multiclass segmentation
+#     :param reference_file: Path to ground-truth segmentation file
+#     :param probability_file: Path to saved softmax probability map file --> todo check shape [C,X,Y,Z] or is it [X,Y,Z] for binary case (foreground probability)
+#     :param image_reader_writer: Used for consistent loading across nnUNetv2
+#     :param labels_or_regions: (same object as passed to compute_metrics -->[1] #binary foreground, [(1,2)] #merged regions, [1,2,3] #multiclass)
+#     :param ignore_label: Label in GT to ignore (e.g. padding, undefined), should match ignore_label used during training
+#     :param n_bins: Number of confidence bins for ECE (typically 10-20), more bins = noisier, fewer bins -> smoother but less precise
+#     :param eps: Numerical stability constant -> prevents log(0) in NLL and division-by-zero in ECE bins
+#     :return:
+#     Output structure example:
+#     {
+#         "reference_file": "...",
+#         "probability_file": "...",
+#         "metrics": {
+#             1: {
+#                 "NLL": 0.42,
+#                 "Brier": 0.13,
+#                 "ECE": 0.06,
+#                 "n_voxels": 123456
+#             }
+#         }
+#     }
+#     """
+#
+#     # --- Load GT and probabilities ---
+#     seg_ref, _ = image_reader_writer.read_seg(reference_file)
+#     seg_ref = seg_ref[0]  # remove singleton --> now shape [X,Y,Z] #todo check
+#     #probs, _ = image_reader_writer.read_seg(probability_file)
+#     probs = np.load(probability_file)['probabilities']               # probabilities
+#
+#
+#     # probs can be [C, X, Y, Z] or [X, Y, Z] (binary FG prob), if binary stored as [X,Y,Z], convert to [2,X,Y,Z]
+#     if probs.ndim == seg_ref.ndim: #binary case [X,Y,Z]
+#         probs = np.stack([1.0 - probs, probs], axis=0) # [2,X,Y,Z]
+#
+#     nr_classes = probs.shape[0]
+#
+#     ignore_mask = seg_ref == ignore_label if ignore_label is not None else None
+#
+#     results = {
+#         "reference_file": reference_file,
+#         "probability_file": probability_file,
+#         "metrics": {}
+#     }
+#
+#     # --- Loop over labels / regions (same philosophy as compute_metrics) ---
+#     for r in labels_or_regions:
+#         results["metrics"][r] = {}
+#
+#         # GT mask for this region (bool)
+#         gt_mask = region_or_label_to_mask(seg_ref, r)
+#
+#         if ignore_mask is not None:
+#             # add explicit calculation of valid_voxels -> done in compute_tp_fp_fn_tn implicitly (use_mask)
+#             valid_mask = ~ignore_mask if ignore_mask is not None else np.ones_like(seg_ref, bool)
+#         else:
+#             valid_mask = np.ones_like(gt_mask, dtype=bool)
+#
+#         # only foreground computation by default
+#         eval_mask = gt_mask & valid_mask #bool
+#
+#         if not np.any(eval_mask):
+#             results["metrics"][r]["NLL"] = np.nan
+#             results["metrics"][r]["Brier"] = np.nan
+#             results["metrics"][r]["ECE"] = np.nan
+#             results["metrics"][r]["n_voxels"] = 0
+#             continue
+#
+#         # --- Extract probabilities ---
+#         # Ground truth for selected voxels
+#         gt_voxels = seg_ref[eval_mask].astype(int) # shape [nr_valid_voxels] or  seg_ref[0][eval_mask] to remove singleton [1,X,Y,Z]
+#         # probs has shape [C, D, H, W], keep all classes
+#         probs_eval = probs[:, eval_mask]  # shape [nr_classes, nr_valid_voxels]
+#         # # transpose to match metric function format
+#         probs_eval = probs_eval.T  # shape [nr_valid_voxels, nr_classes] #todo check -> check also that there is no issue with shape
+#
+#         # --- Negative Log-Likelihood (multiclass) ---
+#         p_true = probs_eval[np.arange(gt_voxels.size), gt_voxels] #if probs_eval was transposed
+#         # p_true = probs_eval[gt_voxels, np.arange(gt_voxels.size)] # if probs_eval was not transposed
+#         nll = float(-np.mean(np.log(p_true + eps)))
+#
+#         # --- Brier score (multiclass) ---
+#         y_onehot = np.eye(nr_classes)[gt_voxels] # shape [nr_valid_voxels, nr_classes]
+#         brier = float(np.mean(np.sum((probs_eval - y_onehot) **2, axis=1)))
+#
+#         # --- Expected Calibration Error (multiclass)---
+#         pred = np.argmax(probs_eval, axis=1)
+#         confidence = np.max(probs_eval, axis=1)
+#         correct = pred == gt_voxels
+#
+#         ece = compute_ece(confidence, correct, n_bins=n_bins)
+#
+#         # --- Store results ---
+#         results["metrics"][r]["NLL"] = nll
+#         results["metrics"][r]["Brier"] = brier
+#         results["metrics"][r]["ECE"] = ece
+#         results["metrics"][r]["n_voxels"] = int(eval_mask.sum())
+#
+#     return results
+
 def compute_probabilistic_metrics(
-        reference_file: str,
-        probability_file: str,   # <-- NEW (saved softmax)
-        image_reader_writer: BaseReaderWriter,
-        labels_or_regions,
-        ignore_label=None,
-        n_bins=15,
-        eps: float = 1e-8,
-) -> dict:
-    """
-    This function the compute_metrics equivalent for uncertainty calibration (evaluates the quality of probabilities):
-        - loads GT
-        - loads probability maps
-        - Compute probabilistic / calibration metrics for segmentation::
-            - NLL
-            - Brier Score
-            - ECE
-            Metrics are computed voxel-wise and aggregated per case.
-            Works for binary and multiclass segmentation
-    :param reference_file: Path to ground-truth segmentation file
-    :param probability_file: Path to saved softmax probability map file --> todo check shape [C,X,Y,Z] or is it [X,Y,Z] for binary case (foreground probability)
-    :param image_reader_writer: Used for consistent loading across nnUNetv2
-    :param labels_or_regions: (same object as passed to compute_metrics -->[1] #binary foreground, [(1,2)] #merged regions, [1,2,3] #multiclass)
-    :param ignore_label: Label in GT to ignore (e.g. padding, undefined), should match ignore_label used during training
-    :param n_bins: Number of confidence bins for ECE (typically 10-20), more bins = noisier, fewer bins -> smoother but less precise
-    :param eps: Numerical stability constant -> prevents log(0) in NLL and division-by-zero in ECE bins
-    :return:
-    Output structure example:
-    {
-        "reference_file": "...",
-        "probability_file": "...",
-        "metrics": {
-            1: {
-                "NLL": 0.42,
-                "Brier": 0.13,
-                "ECE": 0.06,
-                "n_voxels": 123456
-            }
-        }
-    }
-    """
-
-    # --- Load GT and probabilities ---
-    seg_ref, _ = image_reader_writer.read_seg(reference_file)
-    seg_ref = seg_ref[0]  # remove singleton --> now shape [X,Y,Z] #todo check
-    #probs, _ = image_reader_writer.read_seg(probability_file)
-    probs = np.load(probability_file)['probabilities']               # probabilities
-
-
-    # probs can be [C, X, Y, Z] or [X, Y, Z] (binary FG prob), if binary stored as [X,Y,Z], convert to [2,X,Y,Z]
-    if probs.ndim == seg_ref.ndim: #binary case [X,Y,Z]
-        probs = np.stack([1.0 - probs, probs], axis=0) # [2,X,Y,Z]
-
-    nr_classes = probs.shape[0]
-
-    ignore_mask = seg_ref == ignore_label if ignore_label is not None else None
-
-    results = {
-        "reference_file": reference_file,
-        "probability_file": probability_file,
-        "metrics": {}
-    }
-
-    # --- Loop over labels / regions (same philosophy as compute_metrics) ---
-    for r in labels_or_regions:
-        results["metrics"][r] = {}
-
-        # GT mask for this region (bool)
-        gt_mask = region_or_label_to_mask(seg_ref, r)
-
-        if ignore_mask is not None:
-            # add explicit calculation of valid_voxels -> done in compute_tp_fp_fn_tn implicitly (use_mask)
-            valid_mask = ~ignore_mask if ignore_mask is not None else np.ones_like(seg_ref, bool)
-        else:
-            valid_mask = np.ones_like(gt_mask, dtype=bool)
-
-        # only foreground computation by default
-        eval_mask = gt_mask & valid_mask #bool
-
-        if not np.any(eval_mask):
-            results["metrics"][r]["NLL"] = np.nan
-            results["metrics"][r]["Brier"] = np.nan
-            results["metrics"][r]["ECE"] = np.nan
-            results["metrics"][r]["n_voxels"] = 0
-            continue
-
-        # --- Extract probabilities ---
-        # Ground truth for selected voxels
-        gt_voxels = seg_ref[eval_mask].astype(int) # shape [nr_valid_voxels] or  seg_ref[0][eval_mask] to remove singleton [1,X,Y,Z]
-        # probs has shape [C, D, H, W], keep all classes
-        probs_eval = probs[:, eval_mask]  # shape [nr_classes, nr_valid_voxels]
-        # # transpose to match metric function format
-        probs_eval = probs_eval.T  # shape [nr_valid_voxels, nr_classes] #todo check -> check also that there is no issue with shape
-
-        # --- Negative Log-Likelihood (multiclass) ---
-        p_true = probs_eval[np.arange(gt_voxels.size), gt_voxels] #if probs_eval was transposed
-        # p_true = probs_eval[gt_voxels, np.arange(gt_voxels.size)] # if probs_eval was not transposed
-        nll = float(-np.mean(np.log(p_true + eps)))
-
-        # --- Brier score (multiclass) ---
-        y_onehot = np.eye(nr_classes)[gt_voxels] # shape [nr_valid_voxels, nr_classes]
-        brier = float(np.mean(np.sum((probs_eval - y_onehot) **2, axis=1)))
-
-        # --- Expected Calibration Error (multiclass)---
-        pred = np.argmax(probs_eval, axis=1)
-        confidence = np.max(probs_eval, axis=1)
-        correct = pred == gt_voxels
-
-        ece = compute_ece(confidence, correct, n_bins=n_bins)
-
-        # --- Store results ---
-        results["metrics"][r]["NLL"] = nll
-        results["metrics"][r]["Brier"] = brier
-        results["metrics"][r]["ECE"] = ece
-        results["metrics"][r]["n_voxels"] = int(eval_mask.sum())
-
-    return results
-
-def compute_probabilistic_metrics_new(
         reference_file: str,
         probability_file: str,   # <-- NEW (saved softmax)
         image_reader_writer: BaseReaderWriter,
@@ -276,20 +276,20 @@ def compute_probabilistic_metrics_new(
         # --- Extract probabilities ---
         # Ground truth for selected voxels
         gt_voxels = seg_ref[eval_mask].astype(int) # shape [nr_valid_voxels] or  seg_ref[0][eval_mask] to remove singleton [1,X,Y,Z]
-        # Flatten spatial dimensions
-        flat_mask = eval_mask.ravel()  # shape [n_voxels]
-        probs_flat = probs.reshape(nr_classes, -1)  # shape [nr_classes, X*Y*Z]
+        # # Flatten spatial dimensions
+        # flat_mask = eval_mask.ravel()  # shape [n_voxels]
+        # probs_flat = probs.reshape(nr_classes, -1)  # shape [nr_classes, X*Y*Z]
+        #
+        # # Now select only the voxels we want
+        # probs_eval = probs_flat[:, flat_mask]
+        # # transpose to match metric function format
+        # probs_eval = probs_eval.T  # shape [n_selected_voxels, nr_classes]
+        #
+        # #todo currently seg_ref does not have the same shape as the probability.
+        # #i.e. X,Y,Z of eval_mask / seg_ref is 208,448,224 and the probs is 176,512,256
 
-        # Now select only the voxels we want
-        probs_eval = probs_flat[:, flat_mask]
-        # transpose to match metric function format
-        probs_eval = probs_eval.T  # shape [n_selected_voxels, nr_classes]
-
-        #todo currently seg_ref does not have the same shape as the probability.
-        #i.e. X,Y,Z of eval_mask / seg_ref is 208,448,224 and the probs is 176,512,256
-
-        # # probs has shape [C, D, H, W], keep all classes
-        # probs_eval = probs[:, eval_mask]  # shape [nr_classes, nr_valid_voxels]
+        # probs has shape [C, D, H, W], keep all classes
+        probs_eval = probs[:, eval_mask]  # shape [nr_classes, nr_valid_voxels]
 
         # # transpose to match metric function format
         probs_eval = probs_eval.T  # shape [nr_valid_voxels, nr_classes] #todo check -> check also that there is no issue with shape
@@ -361,19 +361,7 @@ def compute_probabilistic_metrics_on_folder(folder_ref: str, folder_prob: str, o
     if not chill:
         missing = set(ref_map) - set(prob_map)
         assert not missing, f"Missing probabilistic files for: {missing}"
-        # --- prepare args ---
-    args = zip(
-        aligned_ref,
-        aligned_prob,
-        [image_reader_writer] * len(aligned_prob),
-        [regions_or_labels] * len(aligned_prob),
-        [ignore_label] * len(aligned_prob),
-        [n_bins] * len(aligned_prob),
-        [eps] * len(aligned_prob)
-    )
 
-    # no multiprocessing
-    results = []
 
     args = zip(
         aligned_ref,
@@ -421,10 +409,15 @@ def compute_probabilistic_metrics_on_folder(folder_ref: str, folder_prob: str, o
             values.append(means[k][m])
         foreground_mean[m] = np.mean(values)
 
+    # global mean
+    global_mean = {}
+    for m in metric_list:
+        global_mean[m] = np.nanmean([i['metrics']['global'][m] for i in results])
+
     [recursive_fix_for_json_export(i) for i in results]
     recursive_fix_for_json_export(means)
     recursive_fix_for_json_export(foreground_mean)
-    result = {'metric_per_case': results, 'mean': means, 'foreground_mean': foreground_mean}
+    result = {'metric_per_case': results, 'mean': means, 'foreground_mean': foreground_mean, 'global_mean': global_mean}
     if output_file is not None:
         save_summary_json(result, output_file)
     return result
